6155e1f034a1677aef7eae54be307e8b9f0f3346
mvn23 <schopdiedwaas@gmail.com>
2019-03-12 17:36:50 +0100

    Bump version to 0.4b2 Update setup.py with new filename for README.md

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="pyotgw",
-    version="0.4b1",
+    version="0.4b2",
     author="Milan van Nugteren",
     author_email="milan@network23.nl",
     description=("A library to interface with the opentherm gateway through "
@@ -21,7 +21,7 @@ setup(
     keywords="opentherm gateway otgw",
     url="https://github.com/mvn23/pyotgw",
     packages=['pyotgw'],
-    long_description=read('README'),
+    long_description=read('README.md'),
     install_requires=[
         'pyserial-asyncio',
     ],
