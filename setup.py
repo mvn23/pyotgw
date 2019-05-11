30b3a12106f23405f0095b252c345385cf565d23
mvn23 <schopdiedwaas@gmail.com>
2019-05-11 10:26:58 +0200

    Bump version to 0.4b4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="pyotgw",
-    version="0.4b3",
+    version="0.4b4",
     author="Milan van Nugteren",
     author_email="milan@network23.nl",
     description=("A library to interface with the opentherm gateway through "
