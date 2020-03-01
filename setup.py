c041e64fda061118590892fa9dc23b0c36c1d155
mvn23 <schopdiedwaas@gmail.com>
2020-03-01 16:53:59 +0100

    Bump version to 0.6b0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name="pyotgw",
-    version="0.5b1",
+    version="0.6b0",
     author="Milan van Nugteren",
     author_email="milan@network23.nl",
     description=(
