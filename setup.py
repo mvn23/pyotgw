5b6741add9023cfbd83454f515adee154d7d9f37
mvn23 <schopdiedwaas@gmail.com>
2022-11-20 21:26:52 +0100

    Bump version to 2.1.3 (#51)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name="pyotgw",
-    version="2.1.2",
+    version="2.1.3",
     author="Milan van Nugteren",
     author_email="milan@network23.nl",
     description=(
