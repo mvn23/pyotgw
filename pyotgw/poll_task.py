3ab7fc2388a02515998e9eb1a22083abadcfcc27
mvn23 <schopdiedwaas@gmail.com>
2024-09-18 13:23:30 +0200

    * Add support for self-stopping poll tasks * Add more poll tasks for static values (one-shot until successful) * Add more poll tasks for dynamic values * Update tests

--- pyotgw/poll_task.py
+++ pyotgw/poll_task.py
@@ -20,13 +20,53 @@ _LOGGER = logging.getLogger(__name__)
 class OpenThermPollTaskName(StrEnum):
     """Poll task names."""
 
+    ABOUT = "about"
+    BUILD = "build"
+    CLOCK_SPEED = "clock_speed"
     GPIO_STATE = "gpio_state"
+    RESET_CAUSE = "reset_cause"
     SMART_POWER_MODE = "smart_power_mode"
 
 
 def get_all_poll_tasks(gateway: OpenThermGateway):
     """Get all poll tasks for a gateway."""
     return {
+        OpenThermPollTaskName.ABOUT: OpenThermPollTask(
+            OpenThermPollTaskName.ABOUT,
+            gateway,
+            OpenThermReport.ABOUT,
+            None,
+            lambda: (
+                gateway.status.status[OpenThermDataSource.GATEWAY].get(v.OTGW_ABOUT)
+                is None
+            ),
+            60,
+            stop_on_success=True,
+        ),
+        OpenThermPollTaskName.BUILD: OpenThermPollTask(
+            OpenThermPollTaskName.BUILD,
+            gateway,
+            OpenThermReport.BUILD,
+            None,
+            lambda: (
+                gateway.status.status[OpenThermDataSource.GATEWAY].get(v.OTGW_BUILD)
+                is None
+            ),
+            60,
+            stop_on_success=True,
+        ),
+        OpenThermPollTaskName.CLOCK_SPEED: OpenThermPollTask(
+            OpenThermPollTaskName.CLOCK_SPEED,
+            gateway,
+            OpenThermReport.CLOCK_SPEED,
+            None,
+            lambda: (
+                gateway.status.status[OpenThermDataSource.GATEWAY].get(v.OTGW_CLOCKMHZ)
+                is None
+            ),
+            60,
+            stop_on_success=True,
+        ),
         OpenThermPollTaskName.GPIO_STATE: OpenThermPollTask(
             OpenThermPollTaskName.GPIO_STATE,
             gateway,
@@ -48,6 +88,17 @@ def get_all_poll_tasks(gateway: OpenThermGateway):
                     ),
                 )
             ),
+        ),
+        OpenThermPollTaskName.RESET_CAUSE: OpenThermPollTask(
+            OpenThermPollTaskName.RESET_CAUSE,
+            gateway,
+            OpenThermReport.RESET_CAUSE,
+            None,
+            lambda: (
+                gateway.status.status[OpenThermDataSource.GATEWAY].get(v.OTGW_RST_CAUSE)
+                is None
+            ),
+            60,
         ),
         OpenThermPollTaskName.SMART_POWER_MODE: OpenThermPollTask(
             OpenThermPollTaskName.SMART_POWER_MODE,
@@ -80,12 +131,14 @@ class OpenThermPollTask:
         default_values: dict[OpenThermDataSource, dict],
         run_condition: Callable[[], bool],
         interval: float = 10,
+        stop_on_success: bool = False,
     ) -> None:
         """Initialize the object."""
         self._gateway = gateway
         self._interval = interval
         self._report_type = report_type
         self._run_condition = run_condition
+        self._stop_on_success = stop_on_success
         self.default_values = default_values
         self.name = name
 
@@ -99,11 +152,13 @@ class OpenThermPollTask:
         if self.is_running:
             _LOGGER.debug(f"Stopping {self.name} polling routine")
             self._task.cancel()
+            await asyncio.sleep(0)
 
             try:
                 await self._task
             except asyncio.CancelledError:
-                self._gateway.status.submit_full_update(self.default_values)
+                if self.default_values is not None:
+                    self._gateway.status.submit_full_update(self.default_values)
                 _LOGGER.debug(f"{self.name} polling routine stopped")
                 self._task = None
 
@@ -129,4 +184,6 @@ class OpenThermPollTask:
         _LOGGER.debug(f"{self.name} polling routine started")
         while True:
             await self._gateway.get_report(self._report_type)
+            if self._stop_on_success and not self.should_run:
+                asyncio.get_running_loop().create_task(self.stop())
             await asyncio.sleep(self._interval)
